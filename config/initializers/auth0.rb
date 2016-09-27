e5d733f46f4ca104c6e7f76a1e8faaefe543ac1b
yugen305 <danielmenendez01@gmail.com>
2016-09-28 00:19:08 +0200

    add auth0 facebook login

--- config/initializers/auth0.rb
+++ config/initializers/auth0.rb
@@ -1,9 +1,9 @@
-Rails.application.config.middleware.use OmniAuth: :Builder do
-	provider(
-		:auth0,
-		'BeMmGu5eBVZOz2AU3F7MFy1ftH9yID51',
-   	    '80Q9pqaXqfpLQArXj0fLK8QWdEPlJ4siOnblAcWO0vofVzgm31uOTV9MrJAjq7OD',
-        'danielm.eu.auth0.com',
-    	callback_path: "/auth/auth0/callback"
-  	)
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider(
+    :auth0,
+    'BeMmGu5eBVZOz2AU3F7MFy1ftH9yID51',
+    '80Q9pqaXqfpLQArXj0fLK8QWdEPlJ4siOnblAcWO0vofVzgm31uOTV9MrJAjq7OD',
+    'danielm.eu.auth0.com',
+    callback_path: "/auth/auth0/callback"
+  )
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get "/auth/auth0/callback" => "auth0#callback"
+  get "/auth/failure" => "auth0#failure"
+  get "auth/login" => "auth0#login"
+
 	resources :bikes, except: :index
 
   get 'welcome/index'
